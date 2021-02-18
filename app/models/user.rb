f07f778e02e104bb7191bdaf8d035c87aeed28e8
yuki <yuu.maru.571718@gmail.com>
2021-02-18 11:10:42 +0000

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: { maximum: 50 }
+    validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
     has_secure_password
     
     has_many :posts
